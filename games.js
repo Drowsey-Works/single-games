2a3411a4cec00720549a465340289afb6d20463b
DrowseyWorks <lifesonlyalien1@gmail.com>
2026-09-05 12:34:59 -0400

    Update games.js

--- games.js
+++ games.js
@@ -3048,7 +3048,7 @@ function generateAllSections() {
           const encoded = encodeURIComponent(normalized);
 
           fetch(
-            `https://cdn.jsdelivr.net/gh/Drowsey-Wroks/UGS-Files/${encoded}?t=${Date.now()}`,
+            `https://cdn.jsdelivr.net/gh/Drowsey-Works/UGS-Files/${encoded}?t=${Date.now()}`,
           )
             .then((response) => response.text())
             .then((text) => {
